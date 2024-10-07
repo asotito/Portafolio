a25ebe664654f3bef130047b23400917ddc7ed56
asotito <84928376+asotito@users.noreply.github.com>
2024-10-06 19:30:53 -0500

    added certifications section, certification cards with information relevant to my certifications

--- src/data/CertificationCard.ts
+++ src/data/CertificationCard.ts
@@ -1,4 +1,5 @@
 interface CertificationCard {
+    id: string;
     name: string;
     institution: string;
     year: number;
@@ -8,6 +9,7 @@ interface CertificationCard {
 
 export const cards: CertificationCard[] = [
     {
+        id: "1",
         name: 'Machine Learning with TensorFlow on Google Cloud en Español',
         institution: 'Coursera',
         year: 2023,
@@ -15,6 +17,7 @@ export const cards: CertificationCard[] = [
         link: 'https://www.coursera.org/account/accomplishments/specialization/certificate/VLQVK7RPMKMA'
     },
     {
+        id: "2",
         name: 'Python for Everybody',
         institution: 'Coursera',
         year: 2023,
@@ -22,10 +25,35 @@ export const cards: CertificationCard[] = [
         link: 'https://www.coursera.org/account/accomplishments/specialization/certificate/5JY9PSFMSVYE'
     },
     {
+        id: "3",
         name: 'Good with Words: Writing and Editing',
         institution: 'Coursera',
         year: 2022,
         image: '',
         link: 'https://www.coursera.org/account/accomplishments/specialization/certificate/2QM6UUXPC59S'
+    },
+    {
+        id: "4",
+        name: 'Website Oficial ANEIMERA UPC',
+        institution: 'IEEE Cirtcuits and Systems Society UPC',
+        year: 2024,
+        image: '',
+        link: 'https://drive.google.com/file/d/1e7Zn-cTlSHU0ydsacaJz-oPqZbxNNMoV/view'
+    },
+    {
+        id: "5",
+        name: 'JavaScript Flex',
+        institution: 'Coderhouse',
+        year: 2024,
+        image: '',
+        link: 'https://www.coderhouse.com/pe/certificados/66ff1c6bd1c51d20ed8922a7?lang='
+    },
+    {
+        id: "6",
+        name: 'React Js Flex',
+        institution: 'Coderhouse',
+        year: 2024,
+        image: '',
+        link: 'https://www.coderhouse.com/pe/certificados/6702ae89d1c51d20ed8923b9?lang='
     },
 ]
